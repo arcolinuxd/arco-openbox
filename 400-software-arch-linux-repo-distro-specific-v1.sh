1002f6014394d178d6fcf4c3a8c1c5339bcb3cf7
Erik Dubois <erik.dubois@gmail.com>
2018-03-13 09:38:35 +0100

    comparison xfce

--- 400-software-arch-linux-repo-distro-specific-v1.sh
+++ 400-software-arch-linux-repo-distro-specific-v1.sh
@@ -16,10 +16,6 @@ set -e
 # software from AUR (Arch User Repositories)
 # https://aur.archlinux.org/packages/
 
-#giving tmp folder extra gb in order not to run out of disk space while installing software
-#only if you run into issues with that
-#sudo mount -o remount,size=5G,noatime /tmp
-
 echo "DESKTOP SPECIFIC APPLICATIONS"
 
 echo "Installing category Accessories"
@@ -65,16 +61,12 @@ sudo pacman -S volumeicon  --noconfirm --needed
 sudo pacman -S w3m  --noconfirm --needed
 sudo pacman -S xorg-xrandr --noconfirm --needed
 sudo pacman -S xfce4-appfinder --noconfirm --needed
-#sudo pacman -S xfce4-clipman-plugin --noconfirm --needed
 sudo pacman -S xfce4-notifyd --noconfirm --needed
 sudo pacman -S xfce4-power-manager --noconfirm --needed
 sudo pacman -S xfce4-settings --noconfirm --needed
 sudo pacman -S xfce4-screenshooter --noconfirm --needed
 sudo pacman -S xfce4-taskmanager --noconfirm --needed
 
-#Testing if we need xfce4-session
-#sudo pacman -S xfce4-session --noconfirm --needed
-#sudo rm /usr/share/xsessions/xfce.desktop
 
 echo "################################################################"
 echo "####    Software from Arch Linux Repository installed     ######"
