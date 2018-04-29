edb231fd056abfb6391a4213773e002fb1ecf863
Erik Dubois <erik.dubois@gmail.com>
2018-04-29 14:06:04 +0200

    alignment

--- AUR/install-virtualbox-for-linux-lts-kernel-v2.sh
+++ AUR/install-virtualbox-for-linux-lts-kernel-v2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
 ##################################################################################################################
-# Author  : Erik Dubois
-# Website : https://www.erikdubois.be
-# Website :	https://www.arcolinux.info
-# Website :	https://www.arcolinux.com
-# Website :	https://www.arcolinuxd.com
-# Website :	https://www.arcolinuxforum.com
+# Author	:	Erik Dubois
+# Website	:	https://www.erikdubois.be
+# Website	:	https://www.arcolinux.info
+# Website	:	https://www.arcolinux.com
+# Website	:	https://www.arcolinuxd.com
+# Website	:	https://www.arcolinuxforum.com
 ##################################################################################################################
 #
 #   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
