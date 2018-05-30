e1249304a7e7ce463e7c92d1e7b8779bd73ca284
Erik Dubois <erik.dubois@gmail.com>
2018-05-30 13:42:26 +0200

    lock and oblogout

--- 920-fix-oblogout-icons-v1.sh
+++ 920-fix-oblogout-icons-v1.sh
@@ -19,11 +19,6 @@ sudo sed -i 's/xfce4-session-logout/oblogout/g' ~/.config/xfce4/xfconf/xfce-perc
 
 echo "Sardi icons applied when logging out or shutdown or ..."
 
-sudo sed -i 's/buttontheme = oxygen/buttontheme = Sardi-Oblogout/g' /etc/oblogout.conf
-#sudo sed -i 's/'buttontheme = oxygen'/'buttontheme = adeos-branco'/g' /etc/oblogout.conf
-#sudo sed -i 's/'buttontheme = oxygen'/'buttontheme = adeos-branco-mono'/g' /etc/oblogout.conf
-#sudo sed -i 's/'buttontheme = oxygen'/'buttontheme = adeos-branco-cores'/g' /etc/oblogout.conf
-
 echo "################################################################"
 echo "####                  ICONS ARE NOW APPLIED               ######"
 echo "################################################################"
