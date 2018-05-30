eef8fbd441b14df2e73f39cf7d0402e904576286
Erik Dubois <erik.dubois@gmail.com>
2018-05-30 12:45:24 +0200

    100 lightdm

--- 400-software-arch-linux-repo-distro-specific-v1.sh
+++ 400-software-arch-linux-repo-distro-specific-v1.sh
@@ -52,13 +52,11 @@ sudo pacman -S jq --noconfirm --needed
 sudo pacman -S lxappearance --noconfirm --needed
 sudo pacman -S lxrandr --noconfirm --needed
 sudo pacman -S nitrogen --noconfirm --needed
-sudo pacman -S oblogout --noconfirm --needed
 sudo pacman -S obconf --noconfirm --needed
 sudo pacman -S qt4 --noconfirm --needed
 sudo pacman -S rofi --noconfirm --needed
 sudo pacman -S thunar --noconfirm --needed
 sudo pacman -S tint2 --noconfirm --needed
-sudo pacman -S slim --noconfirm --needed
 sudo pacman -S volumeicon  --noconfirm --needed
 sudo pacman -S w3m  --noconfirm --needed
 sudo pacman -S xorg-xrandr --noconfirm --needed
