bf231d83789dd87cb0fb634d7255e7a9e89747f9
Erik Dubois <erik.dubois@gmail.com>
2019-12-08 17:28:01 +0100

    xfce4-terminal added

--- 400-software-arch-linux-repo-distro-specific-v1.sh
+++ 400-software-arch-linux-repo-distro-specific-v1.sh
@@ -65,6 +65,7 @@ sudo pacman -S xfce4-power-manager --noconfirm --needed
 sudo pacman -S xfce4-settings --noconfirm --needed
 sudo pacman -S xfce4-screenshooter --noconfirm --needed
 sudo pacman -S xfce4-taskmanager --noconfirm --needed
+sudo pacman -S xfce4-terminal --noconfirm --needed
 
 echo "################################################################"
 echo "####    Software from Arch Linux Repository installed     ######"
